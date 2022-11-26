31035bccd3039ba3a8cc516378096c0416eb0303
Sakib Rahman <seu.sakib@gmail.com>
2022-11-26 14:40:36 +0600

    first commit

---
+++ src/components/ReminderList.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+interface ReminderListProps {
+  id: number;
+  title: string;
+}
+
+function ReminderList(props) {
+  return <div></div>;
+}
+
+export default ReminderList;
